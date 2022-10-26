d415c9edc84be691bbd1b3d67a28977bfc98e5ab
石头 <2398391057@qq.com>
2022-10-26 11:28:39 +0800

    实现初步框架的搭建

---
+++ UIAuto/run.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+====================================
+@File Name ：run.py.py
+@Time ： 2022/10/26 11:25
+@Program IDE ：PyCharm
+@Create by Author ： stone
+@Describe：
+====================================
+"""
